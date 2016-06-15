07b058f4a1498f1dbf4f416d7cd3d3c905346e29
fabio92rz <fabio92rz@gmail.com>
2016-06-15 12:41:42 +0200

    Just fixed some stuff

--- V Gruppo. Interpolazione Polinomiale/src/interpolationActivity.java
+++ V Gruppo. Interpolazione Polinomiale/src/interpolationActivity.java
@@ -3,47 +3,83 @@
  */
 
 import Jama.Matrix;
+import org.apache.commons.math3.*;
+import org.apache.commons.math3.analysis.polynomials.PolynomialFunctionLagrangeForm;
+import org.apache.commons.math3.analysis.polynomials.PolynomialFunctionNewtonForm;
+import sun.rmi.runtime.Log;
+
+import java.util.Scanner;
 
 public class interpolationActivity {
-    public static double[] findPolynomialFactors (double[] x, double[] y)
-            throws RuntimeException
-    {
-        int n = x.length;
 
-        double[][] data = new double[n][n];
-        double[]   rhs  = new double[n];
+    public static void NewtonConstruction(int n, double x[], double y[], double a[]) {
+        double w, p;
+        a[0] = y[0];
 
-        for (int i = 0; i < n; i++) {
-            double v = 1;
-            for (int j = 0; j < n; j++) {
-                data[i][n-j-1] = v;
-                v *= x[i];
+        for (int k = 1; k <= n; k++) {
+            w = 1;
+            p = 0;
+
+            for (int j = 0; j < k; j++) {
+                p = p + (a[j] * w);
+                w = w * (x[k] - x[j]);
             }
+            a[k] = (y[k] - p) / w;
+        }
+    }
 
-            rhs[i] = y[i];
+    public static double NewtonEvaluation(int n, double xx, double x[], double a[]) {
+        double px, xd;
+        px = a[n];
+
+        for (int k = n - 1; k >= 0; k--) {
+            xd = xx - x[k];
+            px = a[k] + px * xd;
+        }
+        return px;
+    }
+
+    public static void arrayInterval(double[] v, double n, double a, double b){
+
+        double c = (b - a)/(n - 1.0);
+        v[0] = a;
+
+        for (int i = 1; i<v.length; i++){
+
+            v[i] = v[i - 1] + c;
+        }
+    }
+
+    public static void arrayFormula(double[] v, double n, double a, double b){
+
+        for (int i = 0; i<v.length; i++){
+
+            v[i] = ((b-a)*Math.cos(((2.0*(n-1.0-i)+1.0)*(3.14159))/(2.0*(n)))+(b+a))/2.0;
         }
 
-        // Solve m * s = b
+        for (int j = 0; j<v.length; j++){
 
-        Matrix m = new Matrix (data);
-        Matrix b = new Matrix (rhs, n);
-
-        //lel
-
-        Matrix s = m.solve (b);
-
-        return s.getRowPackedCopy();
+            System.out.print(v[j]);
+        }
     }
 
 
-    public static void main (String args[])
-    {
-        double x[] = {2.0, 1.0, 3.0};
-        double y[] = {3.0, 4.0, 7.0};
+    public static void main(String args[]) {
 
-        double f[] = interpolationActivity.findPolynomialFactors (x, y);
+        double n;
+        double[] v = new double[200];
 
-        for (int i = 0; i < 3; i++)
-            System.out.println (f[i]);
+        double a = 2;
+        double b = 3;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Inserire grado del polinomio, compreso tra 5 e 25");
+        n = input.nextDouble();
+
+        arrayInterval(v, n, a, b);
+        arrayFormula(v, n, a, b);
+
+
     }
 }
